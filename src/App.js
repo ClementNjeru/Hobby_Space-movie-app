03b2c0d9c58e6d000fb68d656e3bc24b7a527258
ClementNjeru <njeruclement36@gmail.com>
2022-09-07 02:38:32 +0300

    added dependancies in the package.json file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Reviews from "./components/Reviews";
+import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
+function App (){
 
-function App() {
-  return (
+    return (
+      <>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/login" element={< Login/>}></Route>
+          <Route path="/reviews" element={<  Reviews/>}></Route>
+          <Route path="/contact" element={<  Contact/>}></Route>
+          {/* <Route path="/footer" element={<  Footer/>}></Route> */}
+
+        </Routes>
     </div>
+    </>
   );
 }
 
