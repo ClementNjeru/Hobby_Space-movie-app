18b716d958481ea2b278bff6f790ad89774fb546
ClementNjeru <njeruclement36@gmail.com>
2022-09-10 17:38:00 +0300

    updates the root folder

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from "./components/About";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Reviews from "./components/Reviews";
 import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
 function App (){
 
     return (
@@ -17,12 +15,10 @@ function App (){
       <Navbar />
         <Routes>
           <Route path="/" element={<Home/>}></Route>
-          <Route path="/about" element={<About/>}></Route>
+          <Route path="/reviews" element={<  Reviews/>}></Route>
           <Route path="/login" element={< Login/>}></Route>
           <Route path="/reviews" element={<  Reviews/>}></Route>
           <Route path="/contact" element={<  Contact/>}></Route>
-          {/* <Route path="/footer" element={<  Footer/>}></Route> */}
-
         </Routes>
     </div>
     </>
